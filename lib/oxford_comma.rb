fb74a9aef2c5976ba838a5b406c84f5d000eb788
Carlos Polanco <carlospolanco@Carloss-Mac-mini.local>
2021-06-29 10:41:39 -0400

    commit tree c5b93ad7681b47c1757c8bbd682212161c45108a

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    if array.size == 1
+        array[0]
+  
+
+    elsif array.size == 2
+        array.join(" and ")
+
+    elsif array.size == 3
+        last = array.pop
+        array.join(", ") + ", and " + last.to_s
+    else
+        last = array.pop
+        array.join(", ") + ", and " + last.to_s
+    end
 
 end
